143363f3f011b77019c5b0d5eaeea6854aaffc34
AnthonyAl-Rifai <anthonyalrifai@gmail.com>
2025-07-13 15:08:32 -0700

    change Full Stack Developer to single span.

--- app/components/HeroPage.tsx
+++ app/components/HeroPage.tsx
@@ -1,15 +1,16 @@
+"use client";
+
 export default function HeroPage() {
   return (
     <>
-      <h1 className="text-6xl font-bold row-start-2 col-span-full">
+      <h1 className="text-6xl font-bold row-start-2 col-span-full row-span-full sm:flex md:block">
         <span className="block overflow-hidden">
-          <span className="block animate-reveal-from-bottom">Full Stack</span>
-        </span>
-        <span className="block overflow-hidden">
-          <span className="block animate-reveal-from-bottom">Developer</span>
+          <span className="block overflow-hidden animate-reveal-from-bottom">
+            Full Stack Developer
+          </span>
         </span>
       </h1>
-      <p className="text-xl font-bold col-start-2 row-start-6 col-span-full">
+      <p className="text-xl font-bold col-start-2 row-start-6 col-span-full md:col-start-3 md:row-start-4">
         <span className="block animate-reveal-from-bottom-delayed">
           Design-minded, detail-obsessed, always curious.
         </span>
