5567f9f31df9e869bce14447dc59563c3b4882a3
AnthonyAl-Rifai <anthonyalrifai@gmail.com>
2025-07-14 23:44:19 -0700

    add hasPadding prop to GridSection.

--- app/components/GridSection.tsx
+++ app/components/GridSection.tsx
@@ -3,6 +3,7 @@ interface GridSectionProps {
   children: React.ReactNode;
   isLandscape?: boolean;
   fillParent?: boolean;
+  hasPadding?: boolean;
 }
 
 export default function GridSection({
@@ -10,15 +11,17 @@ export default function GridSection({
   children,
   isLandscape = false,
   fillParent = true,
+  hasPadding = true,
 }: GridSectionProps) {
   return (
     <div
       className={`
         w-full
         ${fillParent ? "flex-1 h-0 min-h-0" : "h-full"}
+        ${hasPadding ? "p-4" : ""}
         grid
         ${isLandscape ? "grid-cols-5" : "grid-cols-4"}
-        grid-rows-10 gap-4 p-4
+        grid-rows-10 gap-4
         md:grid-cols-8 md:grid-rows-12
         lg:grid-cols-16 lg:grid-rows-6
         ${className}
