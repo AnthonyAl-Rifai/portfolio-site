10302456d3d46119f2d029c8c7a765dcd58ad226
AnthonyAl-Rifai <anthonyalrifai@gmail.com>
2025-07-18 01:49:23 -0700

    delayed the Title animation in HeroSection.

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -37,7 +37,7 @@ export default function HeroSection() {
               initial={{ y: "100%", opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               exit={{ opacity: 0, transition: { duration: 0.2 } }} // Faster fade out
-              transition={{ duration: 0.6, ease: "easeOut" }}
+              transition={{ delay: 0.5, duration: 0.6, ease: "easeOut" }}
               style={{ display: "block" }}
             >
               {titles[currentTitleIndex]}
