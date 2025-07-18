e9285e8971a7381550d753725cab4376a80b6cd6
AnthonyAl-Rifai <anthonyalrifai@gmail.com>
2025-07-17 23:47:09 -0700

    make Section component more flexible.

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -4,27 +4,24 @@ interface SectionProps {
   id: string;
   className?: string;
   children: React.ReactNode;
+  /** Use dvh instead of vh */
   useDvhHeight?: boolean;
 }
 
 const Section = forwardRef<HTMLElement, SectionProps>(
   ({ id, className = "", children, useDvhHeight = false }, ref) => {
-    const heightClass = useDvhHeight
-      ? "h-[calc(100dvh-var(--layout-size))]"
-      : "h-[calc(100vh-var(--layout-size))]";
-
-    const minHeightClass = "min-h-[600px]"; // prevent squish in mobile landscape
+    const usesCustomHeight = className.includes("h-[");
+    const heightClass = usesCustomHeight
+      ? ""
+      : useDvhHeight
+        ? "h-[calc(100dvh-var(--layout-size))]"
+        : "h-[calc(100vh-var(--layout-size))]";
 
     return (
       <section
         ref={ref}
         id={id}
-        className={`
-          w-full flex flex-col
-          ${heightClass}
-          ${minHeightClass}
-          ${className}
-        `}
+        className={`w-full flex flex-col ${heightClass} ${className}`}
         style={{ scrollMarginTop: "var(--layout-size)" }}
       >
         {children}
