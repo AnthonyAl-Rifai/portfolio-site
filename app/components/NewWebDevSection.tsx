a1b3ae22dd3f548aa977fc69edbdb3c521c7392a
AnthonyAl-Rifai <anthonyalrifai@gmail.com>
2025-08-04 23:46:02 -0700

    WIP NewWebDevSection.

--- app/components/NewWebDevSection.tsx
+++ app/components/NewWebDevSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { useInView } from "motion/react";
 import { motion } from "motion/react";
 import clsx from "clsx";
@@ -13,9 +13,11 @@ import MenuIconARight from "../icons/MenuIconARight";
 import { useLayout } from "../context/LayoutContext";
 import { useIsLargerThanMobile } from "../hooks/useIsLargerThanMobile";
 import SectionTitle from "./SectionTitle";
+
 export default function NewWebDevSection() {
   const isLargerThanMobile = useIsLargerThanMobile();
   const { isMobileLandscape } = useLayout();
+
   const projectIds = [
     { id: "suaw", label: "Shut Up & Write!", shortLabel: "SUAW" },
     { id: "dbspy", label: "dbSpy", shortLabel: "dbSpy" },
@@ -23,6 +25,13 @@ export default function NewWebDevSection() {
     { id: "sb1", label: "SB-1 Audio Player", shortLabel: "SB-1" },
     { id: "skills", label: "Skills", shortLabel: "Skills" },
   ];
+
+  const projectRef = useRef(null);
+  const topNavVisible = useInView(projectRef);
+
+  useEffect(() => {
+    console.log("top nav is in view: ", topNavVisible);
+  }, [topNavVisible]);
 
   const suawRef = useRef(null);
   const dbspyRef = useRef(null);
@@ -54,12 +63,8 @@ export default function NewWebDevSection() {
         "md:grid-cols-[1fr_4fr] md:grid-rows-[var(--layout-size)_1fr]"
       )}
     >
-      {/* Sticky Header */}
-      {/* <div className="col-span-full border-b flex items-center p-4 sticky top-[var(--layout-size)] bg-white z-20">
-        <h1 className="text-4xl font-bold">Web Dev</h1>
-      </div> */}
-      <SectionTitle name="Web Dev" isSticky />
-      {/* Sidebar or TopNav */}
+      <SectionTitle name="Development" isSticky showBottomBorder={false} />
+
       {isLargerThanMobile ? (
         <div className="sticky z-10 top-[calc(2*var(--layout-size))] h-[calc(100dvh-2*var(--layout-size))] w-full grid grid-rows-5 border-r bg-white">
           {projectIds.map(({ id, label }, index) => (
@@ -89,8 +94,11 @@ export default function NewWebDevSection() {
           ))}
         </div>
       ) : (
-        <div
-          className="grid grid-cols-5 border-b sticky bg-white z-10 h-[calc(var(--layout-size)/2)]"
+        <motion.div
+          initial={{ x: "-100%" }}
+          animate={topNavVisible ? { x: 0 } : { x: "-100%" }}
+          transition={{ duration: 0.5, ease: "easeOut", delay: 0.2 }}
+          className="grid grid-cols-5 border-y sticky bg-white z-30 h-[calc(var(--layout-size)/2)]"
           style={{
             top: isMobileLandscape
               ? "var(--layout-size)"
@@ -102,8 +110,9 @@ export default function NewWebDevSection() {
               key={id}
               href={`#${id}`}
               className={clsx(
-                "flex items-center justify-center px-2 border-r text-sm font-bold transition-colors",
-                activeIndex === index ? "bg-gray-200" : ""
+                "flex items-center justify-center px-2 text-sm font-bold transition-colors",
+                index !== projectIds.length - 1 && "border-r",
+                activeIndex === index && "bg-gray-200"
               )}
               whileHover={{ scale: 0.95 }}
               whileTap={{ scale: 0.98 }}
@@ -111,11 +120,10 @@ export default function NewWebDevSection() {
               {shortLabel}
             </motion.a>
           ))}
-        </div>
+        </motion.div>
       )}
 
-      {/* Project Content */}
-      <div className="md:col-start-2 border-b">
+      <div ref={projectRef} className="md:col-start-2 border-b">
         <div
           id="suaw"
           ref={suawRef}
