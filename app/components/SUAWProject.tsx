55b830f585d30b377d64ba58a1f58059c0660571
AnthonyAl-Rifai <anthonyalrifai@gmail.com>
2025-08-05 18:04:29 -0700

    update all breakpoint layouts of SuawProject.

--- app/components/SUAWProject.tsx
+++ app/components/SUAWProject.tsx
@@ -6,6 +6,7 @@ import { useLayout } from "../context/LayoutContext";
 import Image from "next/image";
 import MenuIconAUpsideDown from "../icons/MenuIconAUpsideDown";
 import { useRef } from "react";
+import SectionTitle from "./SectionTitle";
 
 export default function SuawProject() {
   const { isMobileLandscape } = useLayout();
@@ -17,7 +18,7 @@ export default function SuawProject() {
     <div
       className={clsx(
         "min-h-screen flex flex-col",
-        isMobileLandscape ? "px-4 pb-8 gap-8 mb-4" : "px-4 gap-16 pb-16"
+        isMobileLandscape ? "px-4 pb-8 gap-8 mb-4" : "px-4 gap-16 pb-16 lg:px-8"
       )}
     >
       {/* Hero Section */}
@@ -26,12 +27,8 @@ export default function SuawProject() {
           "relative flex flex-col items-center text-center",
           isMobileLandscape
             ? "h-auto min-h-screen gap-8"
-            : "h-[calc(100vh-3*var(--layout-size))] justify-center gap-16"
+            : "h-[calc(100vh-2*var(--layout-size))] min-h-0 justify-center gap-16 pb-20 md:pb-32"
         )}
-        // initial={{ y: 200, opacity: 0 }}
-        // whileInView={{ y: 0, opacity: 1 }}
-        // transition={{ duration: 0.8 }}
-        // viewport={{ once: true, amount: 0 }}
       >
         <motion.div
           initial={{ scale: 0, opacity: 0 }}
@@ -41,15 +38,15 @@ export default function SuawProject() {
           <Image
             src="/SUAW_Logo_Main_Yellow.png"
             alt="Shut Up & Write! Logo"
-            width={isMobileLandscape ? 300 : 400} // smaller width in landscape
-            height={isMobileLandscape ? 150 : 200} // maintain aspect ratio
-            className="mx-auto"
+            width={600}
+            height={300}
+            className="mx-auto w-[300px] md:w-[400px] lg:w-[600px] h-auto"
             priority
           />
         </motion.div>
 
         <motion.p
-          className="text-2xl text-black max-w-3xl"
+          className="text-2xl md:text-3xl md:px-8 lg:px-24 lg:text-4xl"
           initial={{ y: 50, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ delay: 0.4 }}
@@ -61,7 +58,9 @@ export default function SuawProject() {
         <div
           className={clsx(
             "absolute",
-            isMobileLandscape ? "bottom-16" : "bottom-0"
+            isMobileLandscape
+              ? "bottom-16"
+              : "bottom-24 md:bottom-32 lg:bottom-16"
           )}
         >
           <AnimatePresence>
@@ -91,24 +90,24 @@ export default function SuawProject() {
       {/* Overview Section */}
       <motion.section
         ref={overviewRef}
-        className="flex flex-col gap-8"
+        className="flex flex-col gap-8 md:gap-12"
         initial={{ y: 100, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.8 }}
         viewport={{ once: true, amount: 0 }}
       >
-        <h2 className="text-3xl font-medium text-black">Project Overview</h2>
-        <div className="border border-black">
+        <SectionTitle name="Project Overview" zIndex={20} removeLeftPadding />
+        <div className="w-full flex justify-center">
           <Image
             src="/suaw-hero-section.png"
             alt="SUAW Hero Section"
             width={800}
             height={400}
-            className="w-full"
+            className="w-full border border-black lg:w-[1200px] lg:mx-auto xl:w-2/3"
             priority
           />
         </div>
-        <p className="text-lg text-black leading-relaxed">
+        <p className="text-lg leading-relaxed md:text-xl lg:text-2xl xl:text-3xl">
           Shut Up & Write! is an initiative of Writing Partners, a California
           based nonprofit organization that helps writers around the world build
           sustainable writing habits through free, peer-led events. Brought on
@@ -121,24 +120,24 @@ export default function SuawProject() {
       </motion.section>
       {/* Key Highlights Section */}
       <motion.section
-        className="flex flex-col gap-8"
+        className="flex flex-col gap-8 md:gap-12"
         initial={{ y: 100, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.8 }}
         viewport={{ once: true, amount: 0 }}
       >
-        <h2 className="text-3xl font-medium text-black">Key Highlights</h2>
-        <div className="border border-black">
+        <SectionTitle name="Key Highlights" zIndex={20} />
+        <div className="w-full flex justify-center">
           <Image
             src="/suaw-online-event-section.png"
             alt="SUAW Online Event Section"
             width={800}
             height={400}
-            className="w-full"
+            className="w-full border border-black lg:w-[1200px] lg:mx-auto xl:w-2/3"
             priority
           />
         </div>
-        <ul className="flex flex-col gap-4">
+        <ul className="flex flex-col gap-4 text-lg md:gap-6 lg:text-2xl xl:text-3xl">
           {[
             "Architected the frontend rebuild with Vue.js and GraphQL, owning development of all core pages for 100,000+ users; partnered with the Executive Director on product strategy, accelerating delivery and reducing release cycles by 33%",
             "Led development of a responsive Vue.js component library with 50+ reusable components; introduced Storybook for faster feedback and configured GitLab CI/CD for versioning, doubling development velocity and eliminating UI inconsistencies",
@@ -147,28 +146,25 @@ export default function SuawProject() {
             "Strengthened API reliability and backend stability by engineering robust PostgreSQL migrations and optimizing Hasura meta-data tracking, ensuring consistent schemas and smooth deployments across staging, production, and local environments",
           ].map((text, i) => (
             <li key={i} className="flex items-start gap-3">
-              <span className="w-2 h-2 bg-black rounded-full mt-[0.5em] flex-shrink-0" />
-              <span className="text-black">{text}</span>
+              <span className="w-2 h-2 bg-black rounded-full mt-[0.6em] flex-shrink-0 xl:mt-[0.5em]" />
+              <span>{text}</span>
             </li>
           ))}
         </ul>
       </motion.section>
       {/* Technology Stack */}
       <motion.section
-        className="flex flex-col items-center gap-8"
+        className="flex flex-col items-center gap-8 md:gap-12"
         initial={{ y: 100, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.8 }}
         viewport={{ once: true }}
       >
-        <h2 className="text-3xl font-medium text-black text-left w-full">
-          Technology Stack
-        </h2>
-
+        <SectionTitle name="Technology Stack" zIndex={20} removeLeftPadding />
         {/* Frontend */}
         <div className="w-full">
-          <h3 className="text-2xl font-medium text-black mb-4">Frontend</h3>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 ">
+          <h3 className="text-2xl font-medium mb-4 lg:text-3xl">Frontend</h3>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 xl:grid-cols-4">
             {[
               { name: "Vue.js" },
               { name: "JavaScript" },
@@ -178,7 +174,7 @@ export default function SuawProject() {
             ].map((tech, index) => (
               <motion.div
                 key={index}
-                className="bg-white border border-black p-6 text-black text-center font-semibold flex justify-center items-center"
+                className="bg-white border border-black p-6 text-center font-semibold flex justify-center items-center"
                 initial={{ scale: 0, opacity: 0 }}
                 whileInView={{ scale: 1, opacity: 1 }}
                 viewport={{ once: true }}
@@ -191,8 +187,8 @@ export default function SuawProject() {
 
         {/* Backend */}
         <div className="w-full">
-          <h3 className="text-2xl font-medium text-black mb-4">Backend</h3>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 ">
+          <h3 className="text-2xl font-medium mb-4 lg:text-3xl">Backend</h3>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 xl:grid-cols-4">
             {[
               { name: "C# .NET" },
               { name: "Hasura" },
@@ -201,7 +197,7 @@ export default function SuawProject() {
             ].map((tech, index) => (
               <motion.div
                 key={index}
-                className="bg-white border border-black p-6 text-black text-center font-semibold flex justify-center items-center"
+                className="bg-white border border-black p-6 text-center font-semibold flex justify-center items-center"
                 initial={{ scale: 0, opacity: 0 }}
                 whileInView={{ scale: 1, opacity: 1 }}
                 viewport={{ once: true }}
@@ -214,10 +210,10 @@ export default function SuawProject() {
 
         {/* DevOps & Tooling */}
         <div className="w-full">
-          <h3 className="text-2xl font-medium text-black mb-4">
+          <h3 className="text-2xl font-medium mb-4 lg:text-3xl">
             DevOps & Tooling
           </h3>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 ">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 xl:grid-cols-4">
             {[
               { name: "Docker" },
               { name: "GitLab CI/CD" },
@@ -230,7 +226,7 @@ export default function SuawProject() {
             ].map((tech, index) => (
               <motion.div
                 key={index}
-                className="bg-white border border-black p-6 text-black text-center font-semibold flex justify-center items-center"
+                className="bg-white border border-black p-6 text-center font-semibold flex justify-center items-center"
                 initial={{ scale: 0, opacity: 0 }}
                 whileInView={{ scale: 1, opacity: 1 }}
                 viewport={{ once: true }}
@@ -243,12 +239,12 @@ export default function SuawProject() {
 
         {/* Testing */}
         <div className="w-full">
-          <h3 className="text-2xl font-medium text-black mb-4">Testing</h3>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
+          <h3 className="text-2xl font-medium mb-4 lg:text-3xl">Testing</h3>
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-6 xl:grid-cols-4">
             {[{ name: "Jest" }].map((tech, index) => (
               <motion.div
                 key={index}
-                className="bg-white border border-black p-6 text-black text-center font-semibold flex justify-center items-center"
+                className="bg-white border border-black p-6 text-center font-semibold flex justify-center items-center"
                 initial={{ scale: 0, opacity: 0 }}
                 whileInView={{ scale: 1, opacity: 1 }}
                 viewport={{ once: true }}
